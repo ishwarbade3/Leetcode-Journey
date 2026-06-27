59c7c1029ad8637dd3c644bfa14a37379b29f4ee
Ishwar Bade <ishwarbade3@gmail.com>
2026-06-27 23:59:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (48.39%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+
+        long long first = LLONG_MIN;
+    long long second = LLONG_MIN;
+    long long third = LLONG_MIN;
+
+    for (int n : nums) {
+        if (n == first || n == second || n == third)
+            continue;
+
+        if (n > first) {
+            third = second;
+            second = first;
+            first = n;
+        } else if (n > second) {
+            third = second;
+            second = n;
+        } else if (n > third) {
+            third = n;
+        }
+    }
+
+    return third == LLONG_MIN ? first : third;
+    }
+};
