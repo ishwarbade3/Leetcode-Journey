20185889c0cd2769a0ff120c6419dd597785f1be
Ishwar Bade <ishwarbade3@gmail.com>
2026-07-02 22:58:11 +0530

    Time: 0 ms (100.00%) | Memory: 146.7 MB (91.74%) - LeetSync

--- 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -12,13 +12,23 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
-       if(root==NULL)return 0;
+    //    if(root==NULL)return 0;
 
-       if(root->left==NULL&&root->right==NULL)return 1;
+    //    if(root->left==NULL&&root->right==NULL)return 1;
 
-       if(root->left==NULL)return minDepth(root->right)+1;
-       if(root->right==NULL)return minDepth(root->left)+1;
+    //    if(root->left==NULL)return minDepth(root->right)+1;
+    //    if(root->right==NULL)return minDepth(root->left)+1;
 
-       return min(minDepth(root->left),minDepth(root->right))+1;  
+    //    return min(minDepth(root->left),minDepth(root->right))+1;  
+
+
+        if (root == nullptr)return 0;
+
+        int left = minDepth(root->left);
+        int right = minDepth(root->right);
+
+      
+        if (left == 0 || right == 0)return left + right + 1;
+        return min(left, right) + 1;
     }
 };
