7716691a707bc8c57a4655e72171b1f47b016bb2
Ishwar Bade <ishwarbade3@gmail.com>
2026-07-25 23:22:26 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (100.00%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MaxProduct(int n) {
+         int first = -1, second = -1;
+
+    while (n != 0) {
+        int digit = n % 10;
+
+        if (digit >= first) {
+            second = first;
+            first = digit;
+        } else if (digit > second) {
+            second = digit;
+        }
+
+        n /= 10;
+    }
+
+    return first * second;
+    }
+}
