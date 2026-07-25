01f4c9c165284c49954e4f9293404312a4652fe9
Ishwar Bade <ishwarbade3@gmail.com>
2026-07-25 23:12:35 +0530

    Time: 44 ms (1.88%) | Memory: 8.7 MB (59.34%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        // int maxnum = INT_MIN;
+        int maxnum = INT_MIN;
+        int temp = n;
+        while (temp != 0) {
+            int digit = temp % 10;
+            temp /= 10;
+            int num = temp;
+            while (num != 0) {
+                int innerdigit = num % 10;
+                num /= 10;
+                maxnum = max(maxnum, digit * innerdigit);
+                cout << maxnum << endl;
+            }
+        }
+
+        return maxnum;
+        //      vector<int> digits;
 
         // while (n != 0) {
-        //     int digit = n % 10;
+        //     digits.push_back(n % 10);
         //     n /= 10;
-        //     int num = n;
-        //     while (num != 0) {
-        //         int innerdigit = num % 10;
-        //         num /= 10;
-        //         maxnum = max(maxnum, digit * innerdigit);
-        //         cout<<maxnum<<endl;
+        // }
+
+        // int maxnum = INT_MIN;
+
+        // for (int i = 0; i < digits.size(); i++) {
+        //     for (int j = i + 1; j < digits.size(); j++) {
+        //         maxnum = max(maxnum, digits[i] * digits[j]);
         //     }
         // }
 
         // return maxnum;
-         vector<int> digits;
-
-    while (n != 0) {
-        digits.push_back(n % 10);
-        n /= 10;
-    }
-
-    int maxnum = INT_MIN;
-
-    for (int i = 0; i < digits.size(); i++) {
-        for (int j = i + 1; j < digits.size(); j++) {
-            maxnum = max(maxnum, digits[i] * digits[j]);
-        }
-    }
-
-    return maxnum;
     }
 };
