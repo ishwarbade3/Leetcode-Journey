86f5baa152eabc19a49bb1891041fcd43150db98
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-02 19:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (68.24%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -35,24 +35,25 @@ public:
 
         //  }
         //  return aSum>bSum;
-         int n = piles.size();
+        //  int n = piles.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        // vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        for (int i = 0; i < n; i++)
-            dp[i][i] = piles[i];
+        // for (int i = 0; i < n; i++)
+        //     dp[i][i] = piles[i];
 
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i <= n - len; i++) {
-                int j = i + len - 1;
+        // for (int len = 2; len <= n; len++) {
+        //     for (int i = 0; i <= n - len; i++) {
+        //         int j = i + len - 1;
 
-                dp[i][j] = max(
-                    piles[i] - dp[i + 1][j],
-                    piles[j] - dp[i][j - 1]
-                );
-            }
-        }
+        //         dp[i][j] = max(
+        //             piles[i] - dp[i + 1][j],
+        //             piles[j] - dp[i][j - 1]
+        //         );
+        //     }
+        // }
 
-        return dp[0][n - 1] > 0;
+        // return dp[0][n - 1] > 0;
+        return true;
     }
 };
