80c7e5bf09caf5239db408ea222f9dfc22fdaa03
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-11 01:12:02 +0530

    Time: 34 ms (73.28%) | Memory: 8.8 MB (93.90%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+          vector<bool> dp(n + 1, false);
+
+        // dp[i] = true if the current player can win with i stones
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                // If removing j*j leaves a losing position,
+                // current player can win.
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
