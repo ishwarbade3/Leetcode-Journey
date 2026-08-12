d64f640378e370398175de43725ef192babba8e5
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-12 23:49:41 +0530

    Time: 15 ms (60.24%) | Memory: 78.7 MB (16.28%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cs
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -1,10 +1,21 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        HashSet<int> ans = new HashSet<int>();
+        // HashSet<int> ans = new HashSet<int>();
 
-        foreach (int n in nums)
-            if(!ans.Add(n))return true;
+        // foreach (int n in nums)
+        //     if(!ans.Add(n))return true;
         
+        // return false;
+          HashSet<int> seen = new HashSet<int>();
+
+        foreach (int num in nums) {
+            if (seen.Contains(num)) {
+                return true;
+            }
+
+            seen.Add(num);
+        }
+
         return false;
     }
 }
