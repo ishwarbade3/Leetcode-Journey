e55463b0520d2ed39b3e6d05bbd4ec61db44802d
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-14 13:16:51 +0530

    Time: 3 ms (46.57%) | Memory: 44.9 MB (56.12%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,23 +1,45 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> arr1 = new HashSet<Integer>();
-        Set<Integer> arr2 = new HashSet<Integer>();
+        // Set<Integer> arr1 = new HashSet<Integer>();
+        // Set<Integer> arr2 = new HashSet<Integer>();
        
 
-        for (int i = 0; i < nums1.length; i++) {
-            arr1.add(nums1[i]);
+        // for (int i = 0; i < nums1.length; i++) {
+        //     arr1.add(nums1[i]);
+        // }
+        // for (int i = 0; i < nums2.length; i++) {
+        //     if (arr1.contains(nums2[i])) {
+        //         arr2.add(nums2[i]);
+        //     }
+        // }
+        // int[] ans = new int[arr2.size()];
+        // int i=0;
+        // for(int num : arr2){
+        //     ans[i]=num;
+        //     i++;
+        // }
+        // return ans;
+          Set<Integer> set = new HashSet<>();
+
+        for (int num : nums1) {
+            set.add(num);
         }
-        for (int i = 0; i < nums2.length; i++) {
-            if (arr1.contains(nums2[i])) {
-                arr2.add(nums2[i]);
+
+        Set<Integer> result = new HashSet<>();
+
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                result.add(num);
             }
         }
-        int[] ans = new int[arr2.size()];
-        int i=0;
-        for(int num : arr2){
-            ans[i]=num;
-            i++;
+
+        int[] ans = new int[result.size()];
+
+        int i = 0;
+        for (int num : result) {
+            ans[i++] = num;
         }
+
         return ans;
     }
 }
