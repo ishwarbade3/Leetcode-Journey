da30ba4bf091f1c992290971bbabe0507c39691f
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-16 23:26:44 +0530

    Time: 7 ms (10.96%) | Memory: 23 MB (56.52%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.cpp
@@ -12,17 +12,34 @@
  */
 class Solution {
 public:
+    // TreeNode* sortedArrayToBST(vector<int>& nums) {
+    //     return arrToBST(nums, 0, nums.size()-1);
+    // }
+    // TreeNode* arrToBST(vector<int>& n, int left, int right) {
+    //     if (left > right)
+    //         return NULL;
+
+    //     int mid = left + (right - left) / 2;
+    //     TreeNode* root = new TreeNode(n[mid]);
+    //     root->left =arrToBST(n, left, mid - 1);
+    //     root->right=arrToBST(n, mid + 1, right);
+
+    //     return root;
+    //}
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return arrToBST(nums, 0, nums.size()-1);
+        return arrToBST(nums, 0, nums.size() - 1);
     }
-    TreeNode* arrToBST(vector<int>& n, int left, int right) {
+
+    TreeNode* arrToBST(vector<int>& nums, int left, int right) {
         if (left > right)
             return NULL;
 
         int mid = left + (right - left) / 2;
-        TreeNode* root = new TreeNode(n[mid]);
-        root->left =arrToBST(n, left, mid - 1);
-        root->right=arrToBST(n, mid + 1, right);
+
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        root->left = arrToBST(nums, left, mid - 1);
+        root->right = arrToBST(nums, mid + 1, right);
 
         return root;
     }
