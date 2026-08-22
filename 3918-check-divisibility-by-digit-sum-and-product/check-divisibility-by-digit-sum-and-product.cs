c2202fe31ab984f33e70f6ebb0cc5a3af22400ff
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-22 23:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 30.8 MB (62.50%) - LeetSync

---
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool CheckDivisibility(int n) {
+         int sum = 0;
+        int product = 1;
+        int num = n;
+
+        while (num != 0) {
+            int digit = num % 10;
+            sum += digit;
+            product *= digit;
+            num /= 10;
+        }
+        if (n % (sum + product) == 0) {
+            return true;
+        }
+        return false;
+    }
+}
