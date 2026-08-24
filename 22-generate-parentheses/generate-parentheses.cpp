83201837031693fdf5f46759d945640b90e40465
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-24 13:28:36 +0530

    Time: 4 ms (31.51%) | Memory: 13 MB (82.92%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -27,11 +27,31 @@ private:
         curr.pop_back();
      }
 
+     //Optimal solution*******
+     void optimalSol(vector<string>& result ,string& curr , int open ,int close,int n){
+        if(curr.length()== 2*n){
+            result.push_back(curr);
+            return;
+        }
+        if(open < n){
+            curr.push_back('(');
+            optimalSol(result,curr,open + 1,close,n);
+            curr.pop_back();
+        }
+        if(close < open){
+            curr.push_back(')');
+            optimalSol(result,curr,open,close + 1,n);
+            curr.pop_back();
+        }
+
+     }
+
 public:
     vector<string> generateParenthesis(int n) {
         vector<string>result;
         string ans="";
-        generateAll(ans,n,result);
+        //generateAll(ans,n,result);
+        optimalSol(result,ans,0,0,n);
         return result;
     }
 };
