f90f9ca342bb5c7fa06dc45e82c5a5e2486d39c7
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-26 21:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (97.37%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cs
+++ 1635-number-of-good-pairs/number-of-good-pairs.cs
@@ -1,15 +1,24 @@
 public class Solution {
     public int NumIdenticalPairs(int[] nums) {
-        int  n = nums.Length;
-        int count= 0;
+        // int  n = nums.Length;
+        // int count= 0;
 
-        for(int i = 0; i < n ; i++){
-            for(int  j = i+1 ; j < n ; j ++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
+        // for(int i = 0; i < n ; i++){
+        //     for(int  j = i+1 ; j < n ; j ++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+        int[] count = new int[101];
+        int pairs = 0;
+
+        foreach (int num in nums) {
+            pairs += count[num];
+            count[num]++;
         }
-        return count;
+
+        return pairs;
     }
 }
