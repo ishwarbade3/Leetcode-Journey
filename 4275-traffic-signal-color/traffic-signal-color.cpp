ab2af23c7bdd12232b71a4b291557a728363438c
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-28 23:09:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (81.90%) - LeetSync

---
+++ 4275-traffic-signal-color/traffic-signal-color.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string trafficSignal(int t) {
+
+        if(t==0)
+        return "Green";
+        else if(t==30)
+        return "Orange";
+        else if(t >= 30 && t <= 90)
+        return "Red";
+        else
+        return "Invalid";
+        
+    }
+};
