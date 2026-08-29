dee8456a20331aeebdfc2a856b43cea1b576a76f
Ishwar Bade <ishwarbade3@gmail.com>
2026-08-29 22:41:24 +0530

    Time: 7 ms (10.77%) | Memory: 263.5 MB (9.79%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -15,9 +15,7 @@ public:
         //     k--;
         // }
           int n = nums.size();
-
         k = k % n;
-
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin() + k);
         reverse(nums.begin() + k, nums.end());
